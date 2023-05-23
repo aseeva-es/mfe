5771751b0074281dbd0ea8c60c509104b17f4329
Elena Aseeva <aseeva.e.s@gmail.com>
2023-05-23 10:39:51 +0300

    fixed components import in container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core';
-import MarketingApp from '../components/MarketingApp';
-import Header from '../components/Header';
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
